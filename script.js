9bb2a56ce62508878eeff3de588370fc6544990b
Ione Alves <99365685+IoneAS22@users.noreply.github.com>
2022-09-13 21:12:27 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+let dias = document.querySelector('.days');
+let horas = document.querySelector('.hours');
+let minutos = document.querySelector('.minutes');
+let segundos = document.querySelector('.seconds');
+
+let todayTimeStamp = new Date().getTime();
+let greysTimeStamp = new Date(2022, 09, 06, 21).getTime();
+let diference = greysTimeStamp - todayTimeStamp;
+
+function greysCountdown () {
+
+    let todayTimeStamp = new Date().getTime();
+    let greysTimeStamp = new Date(2022, 09, 06, 21).getTime();
+
+    let diference = greysTimeStamp - todayTimeStamp;
+
+    dias.innerHTML = `${Math.floor(diference / (1000 * 60 * 60 * 24))}:`;
+    horas.innerHTML = `${Math.floor((diference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))}:`;
+    minutos.innerHTML = `${Math.floor((diference % (1000 * 60 * 60)) / (1000 * 60))}:`;
+    segundosCountdown = Math.floor((diference % (1000 * 60)) / 1000);
+    segundos.innerHTML = `${(segundosCountdown > 9 ? segundosCountdown : `0${segundosCountdown}`)}`;
+
+  if (diference < 0) {
+    document.getElementById("demo").innerHTML = "The New Season Has Already Started. Enjoy!";
+  }
+
+};
+
+setInterval(greysCountdown, 1000)
